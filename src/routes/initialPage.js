f3abcca0e25d3dd6751ea6b454d49878f2ff0262
JuanDi <juandixarteagaperez210@gmail.com>
2022-07-09 23:52:50 -0500

    Se mejora página de inicio

--- src/routes/initialPage.js
+++ src/routes/initialPage.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const html='<h1>Welcome to the jungle</h1>';
+const html =
+  "<div style='display: flex; justify-content: center; align-items: center; margin-top: 30vh; font-family: 'Courier New', Courier, monospace; margin-left: 200px; margin-right: 200px;'><h3>¡Hola!, Bienvenido a la ApiREST del proyecto EdenSoft. <br><br> Esta ApiREST hace las respectivas consultas a la base de datos para enviar los datos al front e interpretarlos, para ingresar a las diferentes tablas, por favor ingrese a las Api.</h3></div>";
 
-router.get("/",(req,res)=>{
-    res.send(html);
-})
+router.get("/", (req, res) => {
+  res.send(html);
+});
 
 module.exports = router; //super importante poner esto porque si no paila la app
-
