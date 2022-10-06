eadf7884a32ce712e41885b81ca1df6a4ad63c25
carlos gonzalez <cmgg7755@gmail.com>
2022-10-06 15:19:40 -0500

    se agrega pedidos del cliente

--- src/routes/pedidos/pedidos/pedidos.js
+++ src/routes/pedidos/pedidos/pedidos.js
@@ -7,8 +7,23 @@ const mySqlConnection = require("../../../conexion");
 //? Traer todos los pedidos =====================================================================================
 router.get("/pedidos", (req, res) => {
   const query =
-    "select * from pedidos where tipo = 'pedido' order by fecha_registro desc";
+    "select p.*, ci.* from pedidos p join clientes_informativos ci on ci.id_cliente_documento = p.id_cliente_documento where tipo = 'pedido' order by fecha_registro desc";
   mySqlConnection.query(query, (err, rows, fields) => {
+    if (!err) {
+      res.send(rows);
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
+//? Traer todos los pedidos por cliente =====================================================================================
+router.get("/pedidos/cliente/:cedula", (req, res) => {
+  const {cedula} = req.params
+  const query =
+    "select p.*, ci.* from pedidos p join clientes_informativos ci on ci.id_cliente_documento = p.id_cliente_documento where tipo = 'pedido' and p.id_cliente_documento= ? order by fecha_registro desc";
+  mySqlConnection.query(query, [cedula], (err, rows, fields) => {
     if (!err) {
       res.send(rows);
     } else {
@@ -21,7 +36,7 @@ router.get("/pedidos", (req, res) => {
 router.get("/pedidos/:id", (req, res) => {
   const { id } = req.params;
   (query =
-    "select p.*, dp.id_pedido as 'id_detalle', dp.id_producto, pr.nombre, dp.cantidad, dp.precio_unitario  from pedidos p join detalle_pedido dp on dp.id_pedido = p.id_pedido join productos pr on pr.id = dp.id_producto where tipo = 'pedido' and dp.id_pedido = ?"),
+    "select p.*, dp.id_pedido as 'id_detalle', dp.id_producto, pr.nombre, dp.cantidad, dp.precio_unitario, concat(ci.nombre,' ',ci.apellido) as cliente, ci.telefono  from pedidos p join clientes_informativos ci on ci.id_cliente_documento = p.id_cliente_documento  join detalle_pedido dp on dp.id_pedido = p.id_pedido join productos pr on pr.id = dp.id_producto where tipo = 'pedido' and dp.id_pedido = ?"),
     [id],
     mySqlConnection.query(query, [id], (err, rows, fields) => {
       if (!err) {
