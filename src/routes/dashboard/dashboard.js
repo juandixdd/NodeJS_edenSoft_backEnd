7877bbcf161bac76841ba6bbd4b7b50e152f29d0
unknown <Sena@MEDAPRCSGFSP826.FormacionSena.RED>
2022-10-19 09:28:31 -0500

    se modificron las querys de productos Dashboard

--- src/routes/dashboard/dashboard.js
+++ src/routes/dashboard/dashboard.js
@@ -8,7 +8,7 @@ const bcryptjs = require("bcrypt");
 //? Get ventas top 
 router.get("/top-productos-ventas", (req, res) => {
     const query =
-    "select dv.*, p.nombre as 'producto',sum(dv.cantidad) as cantidad_ventas from detalle_venta dv join productos p on p.id = dv.id_producto group by dv.id_producto order by cantidad_ventas desc"
+    "select dv.id_producto, p.nombre as 'producto',sum(dv.cantidad) as cantidad_ventas from detalle_venta dv join productos p on p.id = dv.id_producto group by dv.id_producto order by cantidad_ventas desc"
     mySqlConnection.query(query, (err, rows, fields) => {
       if (!err) {
         res.send(rows);
@@ -21,7 +21,7 @@ router.get("/top-productos-ventas", (req, res) => {
   //? Get pedidos top 
 router.get("/top-productos-pedidos", (req, res) => {
     const query =
-   "select dp.*, p.nombre as 'producto', sum(dp.cantidad) as cantidad_ventas from detalle_pedido dp join productos p on p.id = dp.id_producto group by dp.id_producto order by cantidad_ventas desc"
+   "select dp.id_producto, p.nombre as 'producto', sum(dp.cantidad) as cantidad_ventas from detalle_pedido dp join productos p on p.id = dp.id_producto group by dp.id_producto order by cantidad_ventas desc"
     mySqlConnection.query(query, (err, rows, fields) => {
       if (!err) {
         res.send(rows);
