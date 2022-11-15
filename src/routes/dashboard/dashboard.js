7014476a370f44241eb05e3fe3b2a04b8fd9383a
JuanDi <juandixarteagaperez210@gmail.com>
2022-11-15 10:56:48 -0500

    Se agrega api de pedidos locales a dasboard apis

--- src/routes/dashboard/dashboard.js
+++ src/routes/dashboard/dashboard.js
@@ -3,38 +3,55 @@ const router = express.Router();
 const mySqlConnection = require("../../conexion");
 const bcryptjs = require("bcrypt");
 
-
-
-//? Get ventas top 
+//? Get ventas top
 router.get("/top-productos-ventas", (req, res) => {
-    const query =
-    "select dv.id_producto, p.nombre as 'producto',sum(dv.cantidad) as cantidad_ventas from detalle_venta dv join productos p on p.id = dv.id_producto group by dv.id_producto order by cantidad_ventas desc"
-    mySqlConnection.query(query, (err, rows, fields) => {
-      if (!err) {
-        res.send(rows);
-      } else {
-        console.log(err);
-      }
-    });
+  const query =
+    "select dv.id_producto, p.nombre as 'producto',sum(dv.cantidad) as cantidad_ventas from detalle_venta dv join productos p on p.id = dv.id_producto group by dv.id_producto order by cantidad_ventas desc";
+  mySqlConnection.query(query, (err, rows, fields) => {
+    if (!err) {
+      res.send(rows);
+    } else {
+      console.log(err);
+    }
   });
+});
 
-  //? Get pedidos top 
+//? Get pedidos top
 router.get("/top-productos-pedidos", (req, res) => {
-    const query =
-   "select dp.id_producto, p.nombre as 'producto', sum(dp.cantidad) as cantidad_ventas from detalle_pedido dp join productos p on p.id = dp.id_producto group by dp.id_producto order by cantidad_ventas desc"
-    mySqlConnection.query(query, (err, rows, fields) => {
-      if (!err) {
-        res.send(rows);
-      } else {
-        console.log(err);
-      }
-    });
+  const query =
+    "select dp.id_producto, p.nombre as 'producto', sum(dp.cantidad) as cantidad_ventas from detalle_pedido dp join productos p on p.id = dp.id_producto group by dp.id_producto order by cantidad_ventas desc";
+  mySqlConnection.query(query, (err, rows, fields) => {
+    if (!err) {
+      res.send(rows);
+    } else {
+      console.log(err);
+    }
   });
+});
 
-
-  
-
-
-
+//? Get pedidos_lcoales top
+router.get("/top-productos-pedidos-locales", (req, res) => {
+  const query = `
+  select
+    dp.id_producto,
+    p.nombre as 'producto',
+    sum(dp.cantidad) as cantidad_ventas
+  from
+    detalle_pedido_local dp
+  join productos p on
+    p.id = dp.id_producto
+  group by
+    dp.id_producto
+  order by
+    cantidad_ventas desc
+  `;
+  mySqlConnection.query(query, (err, rows, fields) => {
+    if (!err) {
+      res.send(rows);
+    } else {
+      console.log(err);
+    }
+  });
+});
 
 module.exports = router;
