d44a4bdb935e9595f4de017eb4a0c1863cdedd64
unknown <Sena@MEDAPRCSGFSP826.FormacionSena.RED>
2022-11-18 11:32:55 -0500

    se agregan dos rutas de dashboard para la grafica de ventas totale, falta pedidos-Locales

--- src/routes/dashboard/dashboard.js
+++ src/routes/dashboard/dashboard.js
@@ -54,6 +54,11 @@ router.get("/top-productos-pedidos-locales", (req, res) => {
   });
 });
 
+
+//========================================================================================================\\
+
+//----------------------------------cards top 3---------------------------\\
+
 //? Top productos en tabla PEDIDOS
 router.post("/top3-pedidos", (req, res) => {
   const { mes,año } = req.body;
@@ -102,4 +107,60 @@ router.post("/top3-ventas-locales", (req, res) => {
   );
 });
 
+//------------------------------------tabla ventas--------------------------------\\
+
+//? valance diario de ventas segun periodo en tabla PEDIDOS
+router.post("/totales-pedidos", (req, res) => {
+  const { inicio,fin } = req.body;
+  mySqlConnection.query(
+    "select sum(dp.cantidad) as ventas_totales, sum(p.precio_total) as valor_venta, day(fecha_registro) as dia, month(fecha_registro) as mes from pedidos p join detalle_pedido dp on dp.id_pedido = p.id_pedido where fecha_registro between ? and ? and p.tipo ='pedido' group by day(fecha_registro)",
+    [inicio,fin],
+    (err, rows, fields) => {
+      if (!err) {
+        res.send(rows);
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+//? valance diario de ventas segun periodo en tabla PEDIDOS_LOCALES
+router.post("/totales-pedidos-locales", (req, res) => {
+  const { inicio,fin } = req.body;
+  mySqlConnection.query(
+    "select sum(pl.precio_total) as valor_ventas, sum(dpl.cantidad) as ventas_totales from pedido_local pl join detalle_pedido_local dpl on dpl.id_pedido_local = pl.id_pedido_local where fecha_registro between ? and ? and pl.estado =1 group by fecha_registro ",
+    [inicio,fin],
+    (err, rows, fields) => {
+      if (!err) {
+        res.send(rows);
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+//? valance diario de ventas segun periodo en tabla VENTAS_LOCALES
+router.post("/totales-ventas", (req, res) => {
+  const { inicio,fin } = req.body;
+  mySqlConnection.query(
+    "select sum(vl.precio_total) as valor_ventas, sum(dv.cantidad) as ventas_totales, day(fecha_registro) as dia,  month(fecha_registro) as mes from venta_local vl join detalle_venta dv on vl.id_venta = dv.id_venta where fecha_registro between ? and ? and vl.estado = 1 group by fecha_registro",
+    [inicio,fin],
+    (err, rows, fields) => {
+      if (!err) {
+        res.send(rows);
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
 module.exports = router;
+
+
+
+// select sum(p.precio_total) as valor_venta, sum(dp.cantidad) as ventas_totales, day(fecha_registro) as dia, month (fecha_registro)
+// from pedido_local p join detalle_pedido_local dp on dp.id_pedido_local = p.id_pedido_local  
+// where fecha_registro between '2022-11-1' and '2022-11-30' and p.estado =1 group by day(fecha_registro) and month (fecha_registro)
