9ffce385cb4e101f5a19428f293c0c6be90f0dc0
carlos gonzalez <cmgg7755@gmail.com>
2022-11-30 15:14:23 -0500

    se agrega cambiar estado de pedidos locales

--- src/routes/pedidos/pedido_local/pedido_local.js
+++ src/routes/pedidos/pedido_local/pedido_local.js
@@ -32,21 +32,41 @@ router.get("/pedido-local/:id", (req, res) => {
 //? Get all pedido local Data
 router.get("/pedido-local-all/:id", (req, res) => {
   const { id } = req.params;
-  let query =
-    "select dpl.*,p.nombre as 'product_name', p.precio as 'product_price',pl.*,ci.* from detalle_pedido_local dpl join productos p on p.id = dpl.id_producto join pedido_local pl on pl.id_pedido_local = dpl.id_pedido_local join clientes_informativos ci on ci.id_cliente_documento = pl.id_cliente_documento where pl.id_pedido_local  = ?";
-  mySqlConnection.query(query, [id], (err, rows, fields) => {
+  let getAbonoQuery = "select * from abono where id_pedido_local = ?";
+  let queryWithoutAbono =
+    "select dpl.*,p.nombre as 'product_name', p.precio as 'product_price',pl.*,ci.*from detalle_pedio_local dpl join productos p on p.id = dpl.id_producto  join pedido_local pl on pl.id_pedio_local = dpl.id_pedido_local join clientes_informativos ci on ci.id_cliente_documento = pl.id_cliente_documento where pl.id_pedido_local = ?";
+  let queryWithAbono =
+    "select dpl.*,p.nombre as 'product_name', p.precio as 'product_price',pl.*,ci.*, a.valor as 'cantidad_abono', a.estado as 'estado_abono' from detalle_pedido_local dpl join productos p on p.id = dpl.id_producto join pedido_local pl on pl.id_pedido_local = dpl.id_pedido_local join clientes_informativos ci on ci.id_cliente_documento = pl.id_cliente_documento join abono a on a.id_pedido_local = dpl.id_pedido_local where pl.id_pedido_local  = ?";
+  mySqlConnection.query(getAbonoQuery, [id], (err, rows, fields) => {
     if (!err) {
-      res.send(rows);
+      if (rows.length != 0){
+        mySqlConnection.query(queryWithAbono, [id], (err, rows, fields) => {
+          if(!err) {
+            res.send(rows);
+          }else {
+            console.log(err);
+          }
+        });
     } else {
-      console.log(err);
+      mySqlConnection.query(queryWithoutAbono, [id], (err, rows, fields) => {
+        if (!err) {
+          res.send(rows);
+        }else {
+          console.log(err);
+        }
+      });
     }
+   } else {
+    console.log(err);
+   }
   });
+  
 });
 
 
 //? Create data
 router.post("/pedido-local", (req, res) => {
-  const { id_cliente_documento, fecha_registro, fecha_entrega, precio_total, estado } =
+  const { id_cliente_documento, fecha_registro, fecha_entrega, precio_total, estado, pagado } =
     req.body;
   mySqlConnection.query(
     "INSERT INTO pedido_local (id_cliente_documento,fecha_registro, fecha_entrega,precio_total,estado) VALUES (?,?,?,?,?)",
@@ -65,9 +85,43 @@ router.post("/pedido-local", (req, res) => {
   );
 });
 
+//? editData
+router.put("/pedido-local/:id_pedido_local", (req, res) => {
+  const { id_pedido_local } = req.params;
+  const { estado } = req.body;
+  mySqlConnection.query(
+    "UPDATE pedido_local SET estado = ? WHERE id_pedido_local = ?",
+    [estado, id_pedido_local],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({ status: 200, message: "Pedido actualizado" });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
 
-//? Anular una venta
-router.put("/pedido-local/:id_venta", (req, res) => {
+//? editData
+router.put("/pedido-local/abono/:id_pedido_local", (req, res) => {
+  const { id_pedido_local } = req.params;
+  const { estado } = req.body;
+  mySqlConnection.query(
+    "UPDATE abono SET estado = ? WHERE id_pedido_local = ?",
+    [estado, id_pedido_local],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({ status: 200, message: "Abono actualizado" });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+
+//? Anular un pedido
+router.put("/pedido-local/:id_pedido_local", (req, res) => {
     const { estado } = req.body;
     const { id_pedido_local } = req.params;
     let query = "UPDATE pedido_local SET estado = ? WHERE id_pedido_local = ?";
